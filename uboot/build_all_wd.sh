543e2e96f1efd2a13ee56a8510e0d77b3a6e3f1d
Yang Liu <yang.liu@wdc.com>
2016-07-13 02:27:40 +0800

    KAM-1884: Need to set the correct GPIO for monarch/pelican for USB3 power
    fix the type for rtk sata change
    update the USB GPIO setting for Monarch/Pelican USB3 power
    
    Signed-off-by: Yang Liu <yang.liu@wdc.com>

--- uboot/build_all_wd.sh
+++ uboot/build_all_wd.sh
@@ -22,20 +22,17 @@ rm $TARGETDIR/1296_emmc_a01_4ddr4_2GB.uboot32.fb.dvrboot.exe.bin
 
 # SPI
 #
-make distclean;make rtd1295_qa_spi_a01_2ddr3_1GB_config;make clean;make
-cp $DVRBOOTEXE      $TARGETDIR/1295_spi_a01_2ddr3_1GB.uboot32.fb.dvrboot.exe.bin
-#
 make distclean;make rtd1295_qa_spi_a01_2ddr4_2GB_config;make clean;make
 cp $DVRBOOTEXE      $TARGETDIR/1295_spi_a01_2ddr4_2GB.uboot32.fb.dvrboot.exe.bin
+make distclean;make wd_monarch_config;make clean;make
+cp $DVRBOOTEXE      $TARGETDIR/wd_monarch.uboot32.fb.dvrboot.exe.bin
 # eMMC
-#
-make distclean;make rtd1295_qa_emmc_a01_2ddr3_1GB_config;make clean;make
-cp $DVRBOOTEXE      $TARGETDIR/1295_emmc_a01_2ddr3_1GB.uboot32.fb.dvrboot.exe.bin
 #
 make distclean;make rtd1295_qa_emmc_a01_2ddr4_2GB_config;make clean;make
 cp $DVRBOOTEXE      $TARGETDIR/1295_emmc_a01_2ddr4_2GB.uboot32.fb.dvrboot.exe.bin
 #
-make distclean;make rtd1296_qa_emmc_a01_2ddr3_1GB_config;make clean;make
-cp $DVRBOOTEXE      $TARGETDIR/1296_emmc_a01_2ddr3_1GB.uboot32.fb.dvrboot.exe.bin
-#
-ls -l $TARGETDIR/129?_spi_*.bin $TARGETDIR/129?_emmc_*.bin
+make distclean;make wd_pelican_config;make clean;make
+cp $DVRBOOTEXE      $TARGETDIR/wd_pelican.uboot32.fb.dvrboot.exe.bin
+
+
+ls -l $TARGETDIR/129?_spi_*.bin $TARGETDIR/129?_emmc_*.bin $TARGETDIR/wd_*.bin
