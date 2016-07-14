a2617e0386ce2a2b392351371987d6014d2f4917
Yang Liu <yang.liu@wdc.com>
2016-07-14 20:29:50 +0800

    update the bootloader build scripts only for Monarch/Pelican
    Signed-off-by: Yang Liu <yang.liu@wdc.com>

--- uboot/build_all_wd.sh
+++ uboot/build_all_wd.sh
@@ -7,29 +7,14 @@ DVRBOOTEXE=$SCRIPTDIR/examples/flash_writer_u/dvrboot.exe.bin
 #
 mkdir $PWD/out
 #
-rm $TARGETDIR/1295_spi_a00_2ddr3_1GB.uboot32.fb.dvrboot.exe.bin
-rm $TARGETDIR/1295_spi_a00_2ddr4_2GB.uboot32.fb.dvrboot.exe.bin
-rm $TARGETDIR/1295_spi_a01_2ddr3_1GB.uboot32.fb.dvrboot.exe.bin
-rm $TARGETDIR/1295_spi_a01_2ddr4_2GB.uboot32.fb.dvrboot.exe.bin
-rm $TARGETDIR/1296_spi_a01_2ddr3_1GB.uboot32.fb.dvrboot.exe.bin
-rm $TARGETDIR/1295_emmc_a00_2ddr3_1GB.uboot32.fb.dvrboot.exe.bin
-rm $TARGETDIR/1295_emmc_a00_2ddr4_2GB.uboot32.fb.dvrboot.exe.bin
-rm $TARGETDIR/1295_emmc_a01_2ddr3_1GB.uboot32.fb.dvrboot.exe.bin
-rm $TARGETDIR/1295_emmc_a01_2ddr4_2GB.uboot32.fb.dvrboot.exe.bin
-rm $TARGETDIR/1296_emmc_a01_2ddr3_1GB.uboot32.fb.dvrboot.exe.bin
-rm $TARGETDIR/1296_emmc_a01_2ddr4_1GB.uboot32.fb.dvrboot.exe.bin
-rm $TARGETDIR/1296_emmc_a01_4ddr4_2GB.uboot32.fb.dvrboot.exe.bin
+rm $TARGETDIR/wd_monarch.uboot32.fb.dvrboot.exe.bin
+rm $TARGETDIR/wd_pelican.uboot32.fb.dvrboot.exe.bin
 
-# SPI
+# Monarch
 #
-make distclean;make rtd1295_qa_spi_a01_2ddr4_2GB_config;make clean;make
-cp $DVRBOOTEXE      $TARGETDIR/1295_spi_a01_2ddr4_2GB.uboot32.fb.dvrboot.exe.bin
 make distclean;make wd_monarch_config;make clean;make
 cp $DVRBOOTEXE      $TARGETDIR/wd_monarch.uboot32.fb.dvrboot.exe.bin
-# eMMC
-#
-make distclean;make rtd1295_qa_emmc_a01_2ddr4_2GB_config;make clean;make
-cp $DVRBOOTEXE      $TARGETDIR/1295_emmc_a01_2ddr4_2GB.uboot32.fb.dvrboot.exe.bin
+# Pelican
 #
 make distclean;make wd_pelican_config;make clean;make
 cp $DVRBOOTEXE      $TARGETDIR/wd_pelican.uboot32.fb.dvrboot.exe.bin
