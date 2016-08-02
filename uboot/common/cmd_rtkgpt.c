d5a96f4b8b4e4ecd1b6f22cffb8a3cdf5314b553
Yang Liu <yang.liu@wdc.com>
2016-08-01 18:20:09 -0700

    KAM-2128: Need to update the Parition table for Monarch
    update the u-boot parition generation code
    
    Signed-off-by: Yang Liu <yang.liu@wdc.com>

--- uboot/common/cmd_rtkgpt.c
+++ uboot/common/cmd_rtkgpt.c
@@ -96,6 +96,8 @@ void fill_one_pte(gpt_entry * pte_curr, int idx, unsigned long long start_lba, u
     }
 }
 
+#if 0 // for old install_a
+
 void fill_GPT_PTES(gpt_entry * pte_curr, unsigned long long disk_size, unsigned long long start_lba)
 {
     unsigned long long blk_size;
@@ -210,6 +212,106 @@ void fill_GPT_PTES(gpt_entry * pte_curr, unsigned long long disk_size, unsigned 
     blk_size = (disk_size-4)-start_lba;
     fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "USER");pte_curr++;
 }
+
+#endif
+
+#if 1 // for new install_a - 20160716
+void fill_GPT_PTES(gpt_entry * pte_curr, unsigned long long disk_size, unsigned long long start_lba)
+{
+    unsigned long long blk_size;
+    int gpt_idx;
+    gpt_idx = 0;
+
+    // GPT PTE0
+    blk_size = 2014ULL;
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "FW_TABLE");pte_curr++;
+    // --------------------------
+    // GPT PTE1 KERNEL A
+    start_lba += blk_size;
+    blk_size = ((32ULL<<20)>>9);
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "KERNEL");pte_curr++;
+    // GPT PTE2 ROOTFS A
+    start_lba += blk_size;
+    blk_size = ((32ULL<<20)>>9);
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "ROOTFS_NORMAL");pte_curr++;
+    // GPT PTE3 ROOTFS RESCUE
+    start_lba += blk_size;
+    blk_size = ((32ULL<<20)>>9);
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "ROOTFS_RESCUE");pte_curr++;
+    // GPT PTE4  FDT A
+    start_lba += blk_size;
+    blk_size = ((1ULL<<20)>>9);
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "FDT_NORMAL");pte_curr++;
+    // GPT PTE5 FDT RESCUE
+    start_lba += blk_size;
+    blk_size = ((1ULL<<20)>>9);
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "FDT_RESCUE");pte_curr++;
+    // GPT PTE6
+    start_lba += blk_size;
+    blk_size = ((4ULL<<20)>>9);
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "AFW");pte_curr++;
+    // --------------------------
+    // GPT PTE7 
+    start_lba += blk_size;
+    blk_size = ((32ULL<<20)>>9);
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "KERNEL2");pte_curr++;
+    // GPT PTE8 ROOTFS B
+    start_lba += blk_size;
+    blk_size = ((32ULL<<20)>>9);
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "ROOTFS_RESCUE2");pte_curr++;
+    // GPT PTE9 FDT B
+    start_lba += blk_size;
+    blk_size = ((1ULL<<20)>>9);
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "FDT_RESCUE2");pte_curr++;
+    // GPT PTE10
+    start_lba += blk_size;
+    blk_size = ((32ULL<<20)>>9);
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "KERNEL_RESCUE2");pte_curr++;
+    // GPT PTE11 CONFIG
+    start_lba += blk_size;
+    blk_size = ((16ULL<<20)>>9);
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "CONFIG");pte_curr++;
+    // GPT PTE12
+    start_lba += blk_size;
+    blk_size = ((4ULL<<20)>>9);
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "AFW2");pte_curr++;
+    // --------------------------
+    // GPT PTE13
+    start_lba += blk_size;
+    blk_size = ((1ULL<<20)>>9);
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "BOOTCODE32");pte_curr++;
+    // GPT PTE14
+    start_lba += blk_size;
+    blk_size = ((1ULL<<20)>>9);
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "BOOTCODE64");pte_curr++;
+    // GPT PTE15
+    start_lba += blk_size;
+    blk_size = ((1ULL<<20)>>9);
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "BL31");pte_curr++;
+    // GPT PTE16
+    start_lba += blk_size;
+    blk_size = ((1ULL<<20)>>9);
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "BL32");pte_curr++;
+    // --------------------------
+    // GPT PTE17 SYSTEM A
+    start_lba += blk_size;
+    blk_size = ((1024ULL<<20)>>9);
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "SYSTEM");pte_curr++;
+    // --------------------------
+    // GPT PTE18 SYSTEM B
+    start_lba += blk_size;
+    blk_size = ((1024ULL<<20)>>9);
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "SYSTEMB");pte_curr++;
+    // GPT PTE19
+    start_lba += blk_size;
+    blk_size = ((512ULL<<20)>>9);
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "CACHE");pte_curr++;
+    // GPT PTE20
+    start_lba += blk_size;
+    blk_size = (disk_size-4)-start_lba;
+    fill_one_pte(pte_curr, gpt_idx++, start_lba, (start_lba+blk_size-1), "DATA");pte_curr++;
+}
+#endif
 
 unsigned int rtkgpt_gen(int debug_mode)
 {
